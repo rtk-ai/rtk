442274140bf413e8d76e0aba7b2f98d7f6dbb13c
cmolder <28611108+cmolder@users.noreply.github.com>
2026-03-08 15:42:34 -0700

    Move bazel rewrite tests to src/rewrite_cmd.rs

--- src/rewrite_cmd.rs
+++ src/rewrite_cmd.rs
@@ -47,4 +47,57 @@ mod tests {
             Some("rtk git status".into())
         );
     }
+
+    #[test]
+    fn test_bazel() {
+        use std::collections::HashMap;
+
+        // Maps input command -> expected rewritten command
+        let commands = HashMap::from([
+            // bazel build
+            (
+                "bazel build //src:bazel-dev",
+                Some("rtk bazel build //src:bazel-dev"),
+            ),
+            // bazel query
+            ("bazel query //src/...", Some("rtk bazel query //src/...")),
+            (
+                "USE_BAZEL_VERSION=8.2.0 bazel query //src/...",
+                Some("USE_BAZEL_VERSION=8.2.0 rtk bazel query //src/..."),
+            ),
+            // bazel run
+            (
+                "bazel run //src:bazel-dev",
+                Some("rtk bazel run //src:bazel-dev"),
+            ),
+            (
+                "bazel run //src:bazel-dev -- --arg0 --arg1=test -c foo/bar",
+                Some("rtk bazel run //src:bazel-dev -- --arg0 --arg1=test -c foo/bar"),
+            ),
+            // bazel test
+            (
+                "bazel test //src:bazel-dev",
+                Some("rtk bazel test //src:bazel-dev"),
+            ),
+            // bazel passthrough commands
+            ("bazel clean", None),
+            ("bazel clean --expunge", None),
+            ("bazel dump", None),
+            ("bazel help", None),
+            ("bazel -h", None),
+            ("bazel --help", None),
+            ("bazel mod tidy", None),
+            ("bazel print_action //src:bazel-dev", None),
+            ("bazel shutdown", None),
+            ("bazel version", None),
+        ]);
+
+        for (cmd, expected) in commands {
+            let actual = registry::rewrite_command(cmd, &[]);
+            assert_eq!(
+                expected.map(|s| s.to_string()),
+                actual.map(|s| s.to_string())
+            );
+        }
+    }
 }
